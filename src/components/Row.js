c8824e2458196986cd24de458137bd965e1e37bc
torytory11 <ahsys@naver.com>
2023-05-15 15:05:55 +0900

    Update:  add modal img

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,12 @@
 import axios from "../api/axios";
 import React, { useCallback, useEffect, useState } from "react";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -16,7 +19,10 @@ const Row = ({ title, id, fetchUrl }) => {
 
   //useCallback : 컴포넌트가 렌더링되면 함수도 새로 렌더링 된다. 함수는 굳이 다시 생성될 필요가 없기 때문에 usecallback 사용
   //함수를 메모이제이션 해주는 것 usecallback(()=>{},[여기가 바뀔때만 새로 생성하여라 ])
-
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
   return (
     <div>
       <h2>{title}</h2>
@@ -38,7 +44,7 @@ const Row = ({ title, id, fetchUrl }) => {
               className="row__poster"
               src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.name}
-              // onClick={() => handleClick(movie)}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -53,6 +59,9 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </div>
   );
 };
