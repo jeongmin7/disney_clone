5983fcff5fa12dcb8490cb5e44afc1650b6a7637
jeongmin <lee.jeongmin1625@gmail.com>
2023-06-01 16:27:37 +0900

    Feat: 모달창 끄기 구현 useRef를 사용하여 모달창 외의 부분을 선택할 시 모달이 사라지도록 구현

---
+++ src/hooks/useOnClickOutside.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export default function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      // 클릭시 모달창 안이면 그냥 return
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.addEventListener("mousedown", listener);
+      document.addEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
